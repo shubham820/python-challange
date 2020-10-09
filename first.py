b6ae2ec98b2f7dcc84b14d01a672003c4129a86d
com <123@.com>
2020-10-09 09:37:19 +0530

    inital commit

---
+++ first.py
@@ -0,0 +1,9 @@
+class Hactoberfest:
+    def __init__(self,name,age,status):
+        self.name = name
+        # print(self.age)
+        # print(self.status)
+    
+
+ob1 = Hactoberfest("shubham",24,"Single")
+print(ob1.name)
