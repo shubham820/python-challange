7ffe6690aebe9e44716f638c52fc81e2e8a91e68
shubham820 <72184498+shubham820@users.noreply.github.com>
2020-10-09 09:42:38 +0530

    Update first.py
    for hactoberfest

--- first.py
+++ first.py
@@ -1,9 +1,13 @@
 class Hactoberfest:
     def __init__(self,name,age,status):
         self.name = name
+        self.age = age
+        self.status = status
         # print(self.age)
         # print(self.status)
     
 
 ob1 = Hactoberfest("shubham",24,"Single")
 print(ob1.name)
+print(ob1.status)
+print(ob1.age)
